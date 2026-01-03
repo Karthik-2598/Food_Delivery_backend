a514d43b8791dbfa3ba323398edaf306d61b6572
KARRA SAI KARTHIK <Karrasaikarthik2001@gmail.com>
2026-01-03 22:36:08 +0530

    Token issue fix

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -5,9 +5,6 @@ module.exports = (req, res, next) => {
   let token;
    if(req.headers.authorization && req.headers.authorization.startsWith('Bearer ')){
   token = req.headers.authorization?.split(' ')[1];
-   }
-if (!token && req.cookies && req.cookies.token) {
-    token = req.cookies.token;
   }
   if (!token) return res.status(401).json({ error: 'No token provided' });
   try {
