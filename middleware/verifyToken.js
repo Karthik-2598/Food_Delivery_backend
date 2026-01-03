245f04cde738481fcfe8f42cc39599e4c2fa0880
KARRA SAI KARTHIK <Karrasaikarthik2001@gmail.com>
2026-01-03 16:09:40 +0530

    Fix token

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -2,13 +2,17 @@ const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
 
-  let token = req.headers.authorization?.split(' ')[1];
-  if(!token) token = req.cookies.token; // Get from cookie
-  console.log('Token from cookie:', token ? 'Present': 'Missing');
+  let token;
+   if(req.headers.authorization && req.headers.authorization.startsWith('Bearer ')){
+  token = req.headers.authorization?.split(' ')[1];
+   }
+if (!token && req.cookies && req.cookies.token) {
+    token = req.cookies.token;
+  }
   if (!token) return res.status(401).json({ error: 'No token provided' });
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
-   // console.log('Decoded token :', decoded);
+   console.log('Decoded token :', decoded);
     req.user = decoded;
     next();
   } catch (err) {
